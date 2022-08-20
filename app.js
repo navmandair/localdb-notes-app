a94ead49bd0b1ac62c103734fd9c4f84a903c579
Navpreet Singh <mandair96@gmail.com>
2022-08-20 20:22:48 +1200

    Added new note functions

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
 const notes = require('./notes')
+const yargs = require('yargs')
 const validator = require('validator')
 const chalk = require('chalk')
 
-console.log(notes.getNotes())
-console.log(validator.isEmail('t@t.com'))
-console.log(chalk.red.bold('Error!'))
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    description: 'Add new note',
+    builder:{
+        title: {
+            description: 'Note Title',
+            type: 'string',
+            demandOption: true
+        },
+        body: {
+            description: 'Note Description',
+            type: 'string',
+            demandOption: true
+        }
+    },
+    handler: function(argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+
+yargs.command({
+    command: 'remove',
+    description: 'Remove a note',
+    handler: function(argv){
+        console.log('Removing', argv)
+    }
+})
+
+
+yargs.command({
+    command: 'read',
+    description: 'Read a note',
+    handler: function(){
+        console.log('Reading')
+    }
+})
+
+
+yargs.command({
+    command: 'list',
+    description: 'List all notes',
+    handler: function(){
+        console.log('Notes:')
+        let data = notes.getNotes();
+        data.map(note => console.log(note))
+    }
+})
+
+
+yargs.parse()
+
--- notes.js
+++ notes.js
@@ -1,5 +1,45 @@
+const fs = require('fs')
+
+const noteDatabaseFilePath = 'db.json'
+
+
 const getNotes  = function(){
-    return "Your Note";
+    return loadNotes();
 }
 
-module.exports.getNotes = getNotes;
+const addNote = function(title, body){
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title;
+    })
+    console.log(duplicateNotes);
+    if(duplicateNotes.length == 0){
+        notes.push({title, body});
+        saveNotes(notes);
+        console.log('Note added!')
+    }else{
+        console.log('Duplicate note please change title to something else!')
+    }
+    
+}
+
+const loadNotes = function(){
+    try{
+        let stringData = fs.readFileSync(noteDatabaseFilePath).toString()
+        let data = JSON.parse(stringData)
+        return data;
+    } catch(e){
+        return []
+    }
+}
+
+
+const saveNotes = function(notes){
+    let data = JSON.stringify(notes);
+    fs.writeFileSync(noteDatabaseFilePath, data)
+}
+
+module.exports = {
+    addNote: addNote,
+    getNotes: getNotes
+};
